141be2f55b5196ba5c00668bcffe4b47f55be9cb
B0rtleinacan <Cloverlyn123@gmail.com>
2025-05-17 19:47:38 -0500

    did some changes, not a whole lot :(

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 const express = require('express');
 const app = express();
+
+app.use(static('public'));
+
 // Port environment setting
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4002;
 
-app.use(express.static('public'));
+const month = [
+    {
+        id: 'january',
+        amount: 2400
+    },
+    {
+        id: 'febuary',
+        amount: 2400
+    },
+    {
+        id: 'march',
+        amount: 2400
+    },
+    {
+        id: 'april',
+        amount: 2400
+    }
+];
 
-// retieve data from route
-app.get('/', (req, res, next) => {
-    res.send('<h1>This is working!</h1>');
+app.get('/months', (req, res) => {
+    res.status(200).send('Thing happened');
 })
 
-// adds data to route
-app.post('/', (req, res, next) => {
-    res.send('Post request posted');
-})
+app.post('/month', (req, res) => {
+    res.status(201).send('Thing created')
+});
 
+app.put('/month', (req, res) => {
+    res.status(203).send('Thing updated');
+});
+
+app.delete('/month', (req, res) => {
+    res.status(201).send('thing been deleted');
+});
 // server listener
 app.listen(PORT, () => {
     console.log(`Listening to server at port ${PORT}`);
-})
+});
 
 
 
